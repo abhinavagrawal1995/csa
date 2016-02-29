f86b0dd7f4f2495ec16331e40e6e59edc895dc15
Abhinav Agrawal <abhinavagrawal1995@users.noreply.github.com>
2016-02-29 14:09:45 +0530

    Added files via upload

---
+++ csanew/header.php
@@ -0,0 +1,26 @@
+<header id="header">
+	<h1><a href="index.php">CSAconsultants Pvt. Ltd.</a></h1>
+	<nav id="nav">
+		<ul>
+			<li><a href="index.php">Home</a></li>
+			<li>
+				<a href="#" class="icon fa-angle-down">Layouts</a>
+				<ul>
+					<li><a href="generic.php">Generic</a></li>
+					<li><a href="contact.php">Contact</a></li>
+					<li><a href="elements.php">Elements</a></li>
+					<li>
+						<a href="#">Submenu</a>
+						<ul>
+							<li><a href="#">Option One</a></li>
+							<li><a href="#">Option Two</a></li>
+							<li><a href="#">Option Three</a></li>
+							<li><a href="#">Option Four</a></li>
+						</ul>
+					</li>
+				</ul>
+			</li>
+			<li><a href="#" class="button">Sign Up</a></li>
+		</ul>
+	</nav>
+</header>
---
+++ csanew/index.php
@@ -0,0 +1,141 @@
+<!DOCTYPE HTML>
+<!--
+	CSA Consultants pvt. ltd.
+	abhinavagrawal.in | Abhinav Agrawal
+-->
+<html>
+	<head>
+		<title>CSAconsultants Pvt. Ltd.</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+	</head>
+	<body class="landing">
+		<div id="page-wrapper">
+
+			<!-- Header -->
+				<?php require_once('header.php'); ?>
+
+			<!-- Banner -->
+				<section id="banner">
+					<span class="image fit" style="width: 60%;margin: auto;height: 70%;padding-bottom: 2em;"><img src="images/logo.jpg" alt=""></span>
+					<h3>Partnering Best In Breed ERP.</h3>
+					
+				</section>
+
+			<!-- Main -->
+				<section id="main" class="container">
+
+					<section class="box special">
+						<header class="major">
+							<h2>About Us</h2>
+							<p>CSA Consultants Pvt. Ltd. is among Eastern India’s leading Information Technology consultancy, services, and business solution providers. Formed by a group of Chartered Accountants with over 20 years Industry relevant experience in Business IT system consulting. The company is specializing in Enterprise Resource Planning – ROI Assessment/Blueprint/Product and Vendor Selection and Project Management services apart from sales and implementation , Development, Rollout and Support for Oracle and SAP Product Suites. </p>
+						</header>
+						<ul class="actions">
+									<li><a href="#" class="button alt">Learn More</a></li>
+								</ul>
+						<?php include('slide.php'); ?>
+					</section>
+
+					<section class="box special features">
+						<div class="features-row">
+							<section>
+								<span class="icon major fa-bolt accent2"></span>
+								<h3>Consulting</h3>
+								<p>CSA Consultants Pvt. Ltd. is among Eastern India’s leading Information Technology consultancy, services, and business solution providers. Formed by a group of experts with over 15 years in the IT industries with the best of Brands...</p>
+							</section>
+							<section>
+								<span class="icon major fa-area-chart accent3"></span>
+								<h3>SUPPORT</h3>
+								<p>The quality of our project delivery is due to the high level of competence and talent that form our multidisciplinary teams- Finance, Logistic, HR, Production-Process & Discreet, and Database Technology and backed by solid after sales support....</p>
+							</section>
+						</div>
+						<div class="features-row">
+							<section>
+								<span class="icon major fa-cloud accent4"></span>
+								<h3>TRAINING</h3>
+								<p>We look for people with energy, talent, and creative drive, balanced with a commitment to precision and quality. CSA Consultants offers nationally-competitive salaries, benefits, and a working atmosphere that empowers...</p>
+							</section>
+							<section>
+								<span class="icon major fa-lock accent5"></span>
+								<h3>Enim phasellus</h3>
+								<p>Integer volutpat ante et accumsan commophasellus sed aliquam feugiat lorem aliquet ut enim rutrum phasellus iaculis accumsan dolore magna aliquam veroeros.</p>
+							</section>
+						</div>
+					</section>
+
+					<div class="row">
+						<div class="6u 12u(narrower)" style="width:60%;" style="width:70%;">
+
+							<section class="box special" >
+							<h3>Why CSA Consultants</h3>
+								<span class="image featured"><img src="images/whycsa.jpg" alt="" /></span>
+								<!--
+								<p>Integer volutpat ante et accumsan commophasellus sed aliquam feugiat lorem aliquet ut enim rutrum phasellus iaculis accumsan dolore magna aliquam veroeros.</p>
+								<ul class="actions">
+									<li><a href="#" class="button alt">Learn More</a></li>
+								</ul>-->
+							</section>
+
+						</div>
+						<div class="6u 12u(narrower)" style="width:40%;">
+
+							<section class="box special">
+								<?php include('news.php');?>
+							</section>
+
+						</div>
+					</div>
+
+				</section>
+
+			<!-- CTA -->
+				<section id="cta">
+
+					<h2>Sign up for beta access</h2>
+					<p>Blandit varius ut praesent nascetur eu penatibus nisi risus faucibus nunc.</p>
+
+					<form>
+						<div class="row uniform 50%">
+							<div class="8u 12u(mobilep)">
+								<input type="email" name="email" id="email" placeholder="Email Address" />
+							</div>
+							<div class="4u 12u(mobilep)">
+								<input type="submit" value="Sign Up" class="fit" />
+							</div>
+						</div>
+					</form>
+
+				</section>
+
+			<!-- Footer -->
+				<footer id="footer">
+					<ul class="icons">
+						<li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
+						<li><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
+						<li><a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
+						<li><a href="#" class="icon fa-github"><span class="label">Github</span></a></li>
+						<li><a href="#" class="icon fa-dribbble"><span class="label">Dribbble</span></a></li>
+						<li><a href="#" class="icon fa-google-plus"><span class="label">Google+</span></a></li>
+					</ul>
+					<ul class="copyright">
+						<li>&copy; Untitled. All rights reserved.</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+					</ul>
+				</footer>
+
+		</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.dropotron.min.js"></script>
+			<script src="assets/js/jquery.scrollgress.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+			<script src="assets/js/main.js"></script>
+			<?php require_once('slidejs.php'); ?>
+
+	</body>
+</html>
