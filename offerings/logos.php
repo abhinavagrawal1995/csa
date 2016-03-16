165fb3e3448540c94e24e57d3523c39fa418e1c7
abhinavagrawal1995 <abhinavagrawal1995@gmail.com>
2016-03-17 02:24:39 +0530

    viashell

--- offerings/logos.php
+++ offerings/logos.php
@@ -1,7 +1,7 @@
 <div class="box alt 12u">
 	<div class="row no-collapse 50% uniform">
-		<div class="6u"><span class="image fit"><img src="images/ora.jpg" alt="" /></span></div>
-		<div class="3u"><span class="image fit"><img src="images/oracle3.jpg" alt="" /></span></div>
-		<div class="3u"><span class="image fit"><img src="images/ion_logo.jpg" alt="" /></span></div>
-	</div>
-</div>
+		<div class="6u 12u(narrower)"><span class="image fit"><img src="images/ora.jpg" alt="" /></span></div>
+		<div class="3u 7u(narrower)"><span class="image fit"><img src="images/oracle3.jpg" alt="" /></span></div>
+		<div class="3u 5u(narrower)"><span class="image fit"><img src="images/ion_logo.jpg" alt="" /></span></div>
+	</di5>
+</di5>
