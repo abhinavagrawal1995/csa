07e7ced69c89e8c7b3a7f4fa309fddbe916b3905
abhinavagrawal1995 <abhinavagrawal1995@gmail.com>
2016-03-17 21:56:43 +0530

    sad

---
+++ orapractices.php
@@ -0,0 +1,188 @@
+<!DOCTYPE HTML>
+<!--
+	CSA Consultants pvt. ltd.
+	abhinavagrawal.in | Abhinav Agrawal
+-->
+<html>
+	<head>
+		<title>CSAconsultants Pvt. Ltd.</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+	</head>
+	<body>
+		<div id="page-wrapper">
+
+			<!-- Header -->
+				<?php require_once('header.php'); ?>
+
+			<!-- Main -->
+				<section id="main" class="container">
+					<?php include "imghead.php" ?>
+					<div class="box">
+						<h3>Oracle Implementation Practice</h3>
+						<p>CSA Consultants Oracle practice enables customers to achieve cost-effective solutions on their investments in Oracle products through well experienced industry implementation expertise and innovative delivery model. 
+						</p><p>
+						CSA Consultants Oracle Practice Team consists of trained and certified professionals, who possess rich functional experience and technical skills, with full lifecycle implementation experience on various products of Oracle. 
+						</p><p>
+						The most important factor in our practice is our objectives closely aligned with our customers, in addition to quality, lower TCO (Total Cost of Ownership) and rapid implementation. Our Oracle Practice Team helps optimize an organization's investment through a complete set of application lifecycle management services as depicted below.</p>
+
+						<div class="row">
+							<div class="12u">  
+								<section class="box">
+									<span class="image fit">
+										<img src="images/oracle001.jpg" alt="">
+									</span>
+								</section>
+							</div>							
+						</div>
+						<div class="row">
+							<div class="12u">
+								<h3>Oracle Application Implementation Services</h3>
+								<p>CSA Consultants is one of the most preferred partners of Oracle's e-Business suite for implementation, upgrade, maintenance and support services. We offer the following services in Oracle Applications Practice. Our implementation service offering is built on the strength of deep subject matter expertise, strong industry domain & business process knowledge, change management expertise and rich toolkits & templates for risk-free deployment and proven program. We leverage the offshore-onsite model to provide cost savings and an extended pool of experts for the client.</p>
+							</div>							
+						</div>
+						<div class="row">
+							<div class="12u">
+								<h3>Our Approach:</h3>
+								<div class="12u">  
+								<section class="box">
+									<span class="image fit">
+										<img src="images/oracle004.jpg" alt="">
+									</span>
+								</section>
+							</div>
+							</div>							
+						</div>
+						<div class="row">
+							<div class="12u">
+								<h3>Pre-Implementation</h3>
+								<ul>
+									<li>Business process study and optimization</li>
+									<li>Change management</li>
+									<li>IT strategy formulation</li>
+								</ul>
+							</div>							
+						</div>
+						<div class="row">
+							<div class="12u">
+								<h3>Implementation</h3>
+								<ul>
+									<li>Gap analysis and process mapping</li>
+									<li> Configuration</li>
+									<li> Customization, interfacing and integration</li>
+									<li>Migration</li>
+									<li>Training</li>
+									<li>Localization</li>
+								</ul>
+								<p>CSA Consultants as a part of its Implementation Services helps create the architecture, design, and application setup and maps the business processes of the client organization. We follow the Oracle recommended methodology, Application Implementation Methodology (AIM), customized to suit your implementation requirements.</p>
+							</div>
+ 						</div>
+ 						<div class="row">
+							<div class="12u">
+								<h3>Upgrades & Migration Services:</h3>
+								<div class="12u">  
+								<p>CSA Consultants is Upgrade and Migration Services offering have delivered tremendous benefits to our clients worldwide. We have done upgrades of various Oracle Application versions (10.7 NCA, 11.0.3, 11.5.5, 11.5.8) to newer versions, Rel. 12.0.4. CSA Consultants is consultants can help evaluate the need for an upgrade (or re-implementation) on the basis of complexity, investments, expectations of key users and expected benefits from the new version of the application. We can work with you to finalize on a proposed plan and develop a roadmap by assisting you in identifying the advantages and dependencies of the upgrade.</p>
+								<section class="box">
+									<span class="image fit">
+										<img src="images/oracle005.jpg" alt="">
+									</span>
+								</section>
+								</div>
+							</div>							
+						</div>
+						<div class="row">
+							<div class="12u">
+								<h3>Why Upgrade?</h3>
+								<p><b>The typical drivers for an upgrade are: </b>
+								<br>
+								Adaptability and benefit from the new functionality present in the latest version, Release 12 Increased cost of maintenance of the existing version Change of hardware or software platform to newer versions (OS, DB or Application)
+								</p><p>
+								<b>Upgrade and Migration Service Value Props </b>
+								<br>
+								Proven methodology designed on the lines of Oracle recommended Migration Methodology Our expertise worked on more than 30 upgrade and migration success stories of varying degrees of complexity Maximizing offshore leverage to provide significant savings (up to 60%)
+								</p><p>
+								<b>Key Benefits for Enterprises </b>
+								<br>
+								Reduces time, resources and cost of the entire upgrade process Minimizes system downtime during the final production upgrade Increases end-user satisfaction by providing an updated and stable application Opportunity to use new tools & functionality Framework and tools to assist execution and project management Maximizing offshore leverage to provide significant savings (up to 60%) A team of consultants specializing in upgrade services 
+
+								Application Maintenance and Support (AMS) service at CSA Consultants, delivers high-quality 24 X 7 supports for Oracle applications, systems and infrastructure for enterprises. Our Application Maintenance and Support service follows the CSA Consultants Support and Maintenance Model, designed and benchmarked with global standards of support for Oracle applications. The model has a reliable measurement system, improves SLA adherence and has a standardized problem management process that uses the best practices across industries. The support model increases overall reliability, accessibility, performance and effectiveness of applications, while reducing IT maintenance costs. We achieve this through our onsite and offshore delivery models enabling clients to gain maximum business value from their technology investment.</p>
+							</div>							
+						</div>
+						<div class="row">
+							<div class="12u">
+								<h3>Key Benefits for Enterprises</h3>
+								<ul>
+									<li> Improved reliability, availability and performance of the infrastructure, operations, application and interfaces</li>
+									<li> 24 X 7 SLA driven support</li>
+									<li> Framework and tools to assist support execution and project management</li>
+									<li> Maximizing offshore leverage to provide significant savings (up to 60%) and predictable support costs</li>
+									<li> Pool of consultants specialized in providing support services	</li>
+								</ul>
+							</div>							
+						</div>
+						<div class="row">
+							<div class="12u">
+								<h3>Technology Areas</h3>
+								<ul>
+									<li> Post-implementation support</li>
+									<li> 24 X 7 help desk support</li>
+									<li> Problem resolution and knowledge management</li>
+									<li>New customization support and RICE support</li>
+									<li>Applications Database support</li>
+									<li>Testing</li>
+								</ul>
+							</div>							
+						</div>
+						<div class="row">
+							<div class="12u">
+								<h3>Support Value Prop</h3>
+								<ul>
+									<li>SLA based support Pool of consultants specialized in providing support services</li>
+									<li>Maximizing offshore leverage to provide significant savings (up to 60%)</li>
+									<li>Framework and Tools to assist Support Execution and Project Management			</li>
+								</ul>
+							</div>							
+						</div>
+						<div class="row">
+							<div class="12u">
+								<h3>Overall Benefits to the Customer</h3>
+								<ul>
+									<li> Robust and reliable solutions – For developing a new server, storage infrastructure or simply migrating data</li>
+<li> 24x7 global management by experienced, certified Oracle specialists, enabling you to focus on your core business</li>
+<li> Round-the-clock access to Oracle experts (skilled in databases, applications and infrastructure) reducing the need to recruit, train and retain costly personnel</li>
+<li> Integrated service desk ensuring accurate SLA reporting and call-handling</li>
+<li> A defined ‘event correlation and response’ strategy facilitating faster responses to incidents</li>
+<li> Seamless integration provided by our technical application developers and solution architects who can identify application limitations and develop appropriate middleware solutions</li>
+<li> Consolidation and virtualization – thanks to the wealth of experience we’ve gained around the performance issues that cause VM implementations to fail, we can readily identify problems within virtualized environments to ensure failure is never an option.</li>
+								</ul>
+							</div>							
+						</div>
+ Robust and reliable solutions – For developing a new server, storage infrastructure or simply migrating data
+ 24x7 global management by experienced, certified Oracle specialists, enabling you to focus on your core business
+ Round-the-clock access to Oracle experts (skilled in databases, applications and infrastructure) reducing the need to recruit, train and retain costly personnel
+ Integrated service desk ensuring accurate SLA reporting and call-handling
+ A defined ‘event correlation and response’ strategy facilitating faster responses to incidents
+ Seamless integration provided by our technical application developers and solution architects who can identify application limitations and develop appropriate middleware solutions
+ Consolidation and virtualization – thanks to the wealth of experience we’ve gained around the performance issues that cause VM implementations to fail, we can readily identify problems within virtualized environments to ensure failure is never an option.					
+					<?php include "offerings/logos.php"; ?>
+					</div>
+				</section>
+
+			<?php include 'footer.php'; ?>
+
+		</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.dropotron.min.js"></script>
+			<script src="assets/js/jquery.scrollgress.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
