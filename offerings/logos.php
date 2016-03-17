6da2210112c599cfc33aab3d9c777b9bee982dfe
abhinavagrawal1995 <abhinavagrawal1995@gmail.com>
2016-03-18 01:18:08 +0530

    viashell

--- offerings/logos.php
+++ offerings/logos.php
@@ -1,11 +1,11 @@
 <hr>
 <div class="box alt 12u">
 	<div class="row no-collapse 50% uniform">
-		<div class="5u 8u(narrower)" style="margin:auto;"><span class="image fit"><img src="images/ora.jpg" alt="" /></span></div>
-		<div class="2u 4u(narrower)" style="margin:auto;"><span class="image fit"><img src="images/sap.png" alt="" /></span></div>
+		<div class="5u 8u(mobilep)" style="margin:auto;"><span class="image fit"><img src="images/ora.jpg" alt="" /></span></div>
+		<div class="2u 4u(mobilep)" style="margin:auto;"><span class="image fit"><img src="images/sap.png" alt="" /></span></div>
 	
-		<div class="3u 6u(narrower)" style="margin:auto;"><span class="image fit"><img src="images/oracle3.jpg" alt="" /></span></div>
-		<div class="2u 6u(narrower)" style="margin:auto;"><span class="image fit"><img src="images/ion_logo.jpg" alt="" /></span></div>
+		<div class="3u 6u(mobilep)" style="margin:auto;"><span class="image fit"><img src="images/oracle3.jpg" alt="" /></span></div>
+		<div class="2u 6u(mobilep)" style="margin:auto;"><span class="image fit"><img src="images/ion_logo.jpg" alt="" /></span></div>
 	</div>	
 </div>
 <hr>
