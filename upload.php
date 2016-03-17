a4653191c71e31f7313104733f9ad7e945c8e876
abhinavagrawal1995 <abhinavagrawal1995@gmail.com>
2016-03-18 02:58:58 +0530

    viashell

--- upload.php
+++ upload.php
@@ -1,45 +1,17 @@
-<?php 
-	$path="."
-	$filename="README.md"
-   	$file = $path . "/" . $filename;
-    // $file_size = filesize($file);
-    // $handle = fopen($file, "r");
-    // $content = fread($handle, $file_size);
-    // fclose($handle);
-    // $content = chunk_split(base64_encode($content));
-
-    // // a random hash will be necessary to send mixed content
-    // $separator = md5(time());
-
-    // carriage return type (we use a PHP end of line constant)
-    $eol = PHP_EOL;
-
-    // main header (multipart mandatory)
-    $headers = "From: name <test@test.com>" . $eol;
-    $headers .= "MIME-Version: 1.0" . $eol;
-    $headers .= "Content-Type: multipart/mixed; boundary=\"" . $separator . "\"" . $eol;
-    $headers .= "Content-Transfer-Encoding: 7bit" . $eol;
-    $headers .= "This is a MIME encoded message." . $eol;
-
-    // message
-    $headers .= "--" . $separator . $eol;
-    $headers .= "Content-Type: text/plain; charset=\"iso-8859-1\"" . $eol;
-    $headers .= "Content-Transfer-Encoding: 8bit" . $eol;
-    $headers .= $message . $eol;
-
-    // attachment
-    $headers .= "--" . $separator . $eol;
-    $headers .= "Content-Type: application/octet-stream; name=\"" . $filename . "\"" . $eol;
-    $headers .= "Content-Transfer-Encoding: base64" . $eol;
-    $headers .= "Content-Disposition: attachment" . $eol;
-    $headers .= $content . $eol;
-    $headers .= "--" . $separator . "--";
-
-    //SEND Mail
-     // if (mail("abhinavagrawal1995@gmail.com, "Subject1", "", $headers)) {
-     //    echo "mail send ... OK"; // or use booleans here
-     //  } else {
-     //    echo "mail send ... ERROR!";
-     //  }
-
+<?php
+$target_dir = "uploads/";
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$uploadOk = 1;
+$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+// Check if image file is a actual image or fake image
+if(isset($_POST["submit"])) {
+    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+    if($check !== false) {
+        echo "File is an image - " . $check["mime"] . ".";
+        $uploadOk = 1;
+    } else {
+        echo "File is not an image.";
+        $uploadOk = 0;
+    }
+}
 ?>
