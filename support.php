3ef3d1265fc72173c0f0b6457915364d6b71cbbb
abhinavagrawal1995 <abhinavagrawal1995@gmail.com>
2016-03-19 09:29:32 +0530

    viashell

---
+++ support.php
@@ -0,0 +1,79 @@
+<!DOCTYPE HTML>
+<!--
+	CSA Consultants pvt. ltd.
+	abhinavagrawal.in | Abhinav Agrawal
+-->
+<html>
+	<head>
+		<title>CSAconsultants Pvt. Ltd.</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+	</head>
+	<body>
+		<div id="page-wrapper">
+
+			<!-- Header -->
+				<?php require_once('header.php'); ?>
+
+			<!-- Main -->
+				<section id="main" class="container">
+					<?php include "imghead.php" ?>
+					<div class="box">
+						<h3>Support</h3>
+						<div class="row">
+							<div class="12u">
+							<hr>
+							<div class="box alt">
+								<div class="row no-collapse 50% uniform">
+									<div class="3u">Customer support :</div>
+									<div class="9u"><code><a href="http://server.csacons.com:8020">server.csacons.com:8020</a></code></div>
+								</div>
+							</div>
+							<div class="box alt">
+								<div class="row no-collapse 50% uniform">
+									<div class="3u">Web Support :</div>
+									<div class="9u"><code><a href="http://csa.custhelp.com/">csa.custhelp.com</a></code></div>
+								</div>
+							</div>
+							<div class="box alt">
+								<div class="row no-collapse 50% uniform">
+									<div class="3u">Phone No:</div>
+									<div class="9u">
+										<code>+91 98309 24108</code><br>
+										<code>+91 033 2486-2480</code> <br>
+										<code>+91 033 2486-3298</code>
+									</div>
+								</div>
+							</div>							
+							<div class="box alt">
+								<div class="row no-collapse 50% uniform">
+									<div class="3u">Email:</div>
+									<div class="9u">
+										<code>info@csaconsultants.in </code>
+									</div>
+								</div>
+							</div>
+							<hr>
+							</div>						
+						</div>
+					</div>
+				</section>
+
+			<?php include 'footer.php'; ?>
+
+		</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.dropotron.min.js"></script>
+			<script src="assets/js/jquery.scrollgress.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
